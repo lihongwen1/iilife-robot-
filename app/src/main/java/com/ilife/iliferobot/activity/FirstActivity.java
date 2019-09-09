d7123f9aa1ce10ab325ce34ef91951dbd2177cfc
flylinlan <18565713334@163.com>
2019-09-09 14:46:18 +0800

    1.启动页按国内版和国外版区分。不按语言区分！

--- app/src/main/java/com/ilife/iliferobot/activity/FirstActivity.java
+++ app/src/main/java/com/ilife/iliferobot/activity/FirstActivity.java
@@ -4,13 +4,17 @@ import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.accloud.cloudservice.AC;
 import com.badoo.mobile.util.WeakHandler;
 import com.ilife.iliferobot.base.BaseActivity;
 import com.ilife.iliferobot.utils.ToastUtils;
 import com.ilife.iliferobot.R;
+import com.ilife.iliferobot.utils.Utils;
 import com.tbruyelle.rxpermissions2.RxPermissions;
+
+import butterknife.BindView;
 
 
 /**
@@ -20,6 +24,8 @@ import com.tbruyelle.rxpermissions2.RxPermissions;
 public class FirstActivity extends BaseActivity {
     private final String TAG = FirstActivity.class.getSimpleName();
     private final int GOTOMAIN = 0x11;
+    @BindView(R.id.iv_launcher)
+    ImageView iv_launcher;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -40,7 +46,11 @@ public class FirstActivity extends BaseActivity {
 
     @Override
     public void initView() {
-
+        if (Utils.isChinaEnvironment()) {
+             iv_launcher.setImageResource(R.drawable.launcher_page_zh);
+        } else {
+             iv_launcher.setImageResource(R.drawable.launcher_page);
+        }
     }
 
     private WeakHandler handler = new WeakHandler(msg -> {
