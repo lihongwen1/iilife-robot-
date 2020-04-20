bdce7e678596f3a8e4f700b3adfd77b4059ea5ba
linfei <18565713334@163.com>
2020-04-20 17:15:04 +0800

    发布国内V3.3.2

--- app/src/main/java/com/ilife/iliferobot/activity/FirstActivity.java
+++ app/src/main/java/com/ilife/iliferobot/activity/FirstActivity.java
@@ -3,23 +3,20 @@ package com.ilife.iliferobot.activity;
 import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Spannable;
-import android.text.SpannableString;
 import android.text.TextPaint;
 import android.text.style.ClickableSpan;
-import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.accloud.cloudservice.AC;
 import com.badoo.mobile.util.WeakHandler;
-import com.ilife.iliferobot.activity.fragment.UniversalDialog;
+import com.ilife.iliferobot.R;
 import com.ilife.iliferobot.base.BaseActivity;
 import com.ilife.iliferobot.fragment.PrivacyDialogFragment;
 import com.ilife.iliferobot.utils.SpUtils;
 import com.ilife.iliferobot.utils.ToastUtils;
-import com.ilife.iliferobot.R;
 import com.ilife.iliferobot.utils.Utils;
 import com.tbruyelle.rxpermissions2.RxPermissions;
 
@@ -38,6 +35,11 @@ public class FirstActivity extends BaseActivity {
     ImageView iv_launcher;
     @BindView(R.id.tv_slogan)
     TextView tv_slogan;
+
+    @Override
+    protected boolean isChildPage() {
+        return true;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -122,30 +124,6 @@ public class FirstActivity extends BaseActivity {
         }
         if (!protocolDialog.isAdded()) {
             protocolDialog.show(getSupportFragmentManager(), "protocol");
-        }
-    }
-
-    private class MyClickText extends ClickableSpan {
-        private int type;
-
-        public MyClickText(int type) {
-            this.type = type;
-        }
-
-        @Override
-        public void updateDrawState(TextPaint ds) {
-            super.updateDrawState(ds);
-            //设置文本的颜色
-            ds.setColor(getResources().getColor(R.color.color_f08300));
-            //超链接形式的下划线，false 表示不显示下划线，true表示显示下划线
-            ds.setUnderlineText(false);
-        }
-
-        @Override
-        public void onClick(View view) {
-            Intent intent = new Intent(FirstActivity.this, ProtocolActivity.class);
-            intent.putExtra(ProtocolActivity.KEY_TYPE, type);
-            startActivity(intent);
         }
     }
 }
